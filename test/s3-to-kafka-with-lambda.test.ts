f04e3106f844319f62ad5edeb64cc365fc62f149
Anushri Dubey <anushri@Anushris-MacBook-Pro.local>
2021-12-21 21:59:48 +0530

    Initial commit

---
+++ test/s3-to-kafka-with-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3ToKafkaWithLambda from '../lib/s3-to-kafka-with-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-to-kafka-with-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3ToKafkaWithLambda.S3ToKafkaWithLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
